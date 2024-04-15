604efc0d83ba33d875b8b61776bdaa74e4ba16ff
pg9182 <96569817+pg9182@users.noreply.github.com>
2024-04-15 06:02:56 -0400

    plugins/tf2vpk: Update example compile command

--- plugins/tf2vpk/main.go
+++ plugins/tf2vpk/main.go
@@ -9,8 +9,9 @@ import (
 	"github.com/pg9182/7zplugin/z7plugin"
 )
 
-// GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -buildmode=c-shared -ldflags '-extldflags=-static' -o tf2vpk.dll ./plugins/tf2vpk
-// put in Program Files/7-Zip/Formats/ (note: arch must match)
+// CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -buildmode=c-shared -ldflags '-s -w -extldflags=-static' -trimpath -v -x -o tf2vpk64.dll ./plugins/tf2vpk
+// CGO_ENABLED=1 GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc go build -buildmode=c-shared -ldflags '-s -w -extldflags=-static' -trimpath -v -x -o tf2vpk32.dll ./plugins/tf2vpk
+// put the correct one in Program Files/7-Zip/Formats/ (note: arch must match)
 
 func init() {
 	z7plugin.RegisterArchiveHandler(z7plugin.ArchiveHandler{
